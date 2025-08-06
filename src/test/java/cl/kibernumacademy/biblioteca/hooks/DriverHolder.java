787be8627efe4e7331d1baf52bda8a526155cc60
Miguel Ramos <return.infox@gmail.com>
2025-08-05 20:29:15 -0400

    creando el hilo con el webdriver

---
+++ src/test/java/cl/kibernumacademy/biblioteca/hooks/DriverHolder.java
@@ -0,0 +1,26 @@
+package cl.kibernumacademy.biblioteca.hooks;
+
+import org.openqa.selenium.WebDriver;
+
+public class DriverHolder {
+  // almacena un webdriver por hilo, util para ejecución paralela
+  private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+  private static void set(WebDriver webDriver) {
+    driver.set(webDriver);
+  }
+
+  public static WebDriver get() {
+    WebDriver webDriver = driver.get();
+    // webDriver no esta inicializado
+    if(webDriver == null) {
+      throw new IllegalStateException("WebDriver no inicializado");
+    }
+    return webDriver;
+  }
+
+  // Eliminar el webdriver
+  public static void remove() {
+    driver.remove();
+  }
+}
--- src/test/java/cl/kibernumacademy/biblioteca/hooks/Hooks.java
+++ src/test/java/cl/kibernumacademy/biblioteca/hooks/Hooks.java
@@ -1,5 +1,28 @@
 package cl.kibernumacademy.biblioteca.hooks;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Hooks {
   
+  @Before
+  public void setUp() {
+    WebDriver driver = WebDriverManager.chromedriver().create();
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    driver.get("https://biblioteca-testing.netlify.app/");
+  }
+
+
+  @After
+  public void tearDown(Scenario scenario) {
+    
+  }
 }
