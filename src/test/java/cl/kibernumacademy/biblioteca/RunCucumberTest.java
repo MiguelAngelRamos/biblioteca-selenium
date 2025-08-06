faace5224b7b2f1d27f3130090245aa138ab10a1
Miguel Ramos <return.infox@gmail.com>
2025-08-05 21:22:37 -0400

    ejecucion

--- src/test/java/cl/kibernumacademy/biblioteca/RunCucumberTest.java
+++ src/test/java/cl/kibernumacademy/biblioteca/RunCucumberTest.java
@@ -2,11 +2,13 @@ package cl.kibernumacademy.biblioteca;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
+@SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value="cl.kibernumacademy.biblioteca.steps, cl.kibernumacademy.biblioteca.hooks")
 @ConfigurationParameter(
   key = PLUGIN_PROPERTY_NAME,
--- src/test/java/cl/kibernumacademy/biblioteca/steps/LoginSteps.java
+++ src/test/java/cl/kibernumacademy/biblioteca/steps/LoginSteps.java
@@ -1,5 +1,47 @@
 package cl.kibernumacademy.biblioteca.steps;
 
+
+
+import org.junit.jupiter.api.Assertions;
+
+import cl.kibernumacademy.biblioteca.pages.LoginPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class LoginSteps {
-  
+  LoginPage loginPage;
+
+  @Given("que el bibliotecario abre la página de login")
+  public void  que_el_bibliotecario_abre_la_página_de_login() {
+    // new LoginPage().abrir();
+    loginPage = new LoginPage();
+    loginPage.abrir();
+  }
+
+  @When("ingresa credenciales válidas")
+  public void ingresa_credenciales_válidas() {
+    loginPage = new LoginPage();
+    loginPage.login("admin", "secret");
+  }
+
+  @Then("accede al registro de préstamo")
+  public void accede_al_registro_de_préstamo() {
+    loginPage = new LoginPage();
+    Assertions.assertTrue(loginPage.obtenerError().isEmpty(), "No debería haber un mensaje de error");
+  }
+
+  // Login fallido
+  @When("ingresa credenciales inválidas")
+  public void ingresa_credenciales_inválidas() {
+    loginPage = new LoginPage();
+    loginPage.login("userError", "faild");
+  }
+
+  @Then("ve un mensaje de error de login")
+  public void ve_un_mensaje_de_error_de_login() {
+    loginPage = new LoginPage();
+    loginPage.obtenerError().contains(null);
+    Assertions.assertTrue(loginPage.obtenerError().contains("inválidas"), "Debe mostrar mensaje de credenciales inválidas");
+  }
 }
