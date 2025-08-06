8170ff629fb3509b841fe3dd0a3064decaae3e59
Miguel Ramos <return.infox@gmail.com>
2025-08-05 20:15:00 -0400

    mapeo de la pagina de login

--- src/test/java/cl/kibernumacademy/biblioteca/RunCucumberTest.java
+++ src/test/java/cl/kibernumacademy/biblioteca/RunCucumberTest.java
@@ -1,6 +1,18 @@
 package cl.kibernumacademy.biblioteca;
 
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.Suite;
+import static io.cucumber.junit.platform.engine.Constants.*;
 
+@Suite
+@IncludeEngines("cucumber")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value="cl.kibernumacademy.biblioteca.steps, cl.kibernumacademy.biblioteca.hooks")
+@ConfigurationParameter(
+  key = PLUGIN_PROPERTY_NAME,
+  value = "pretty, html:target/cucumber-report.html"
+)
 public class RunCucumberTest {
   
 }
+
--- src/test/java/cl/kibernumacademy/biblioteca/pages/LoginPage.java
+++ src/test/java/cl/kibernumacademy/biblioteca/pages/LoginPage.java
@@ -1,5 +1,36 @@
 package cl.kibernumacademy.biblioteca.pages;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class LoginPage {
-  
+  // Localizadores
+  private By userField = By.id("username");
+  private By passwordField = By.id("password");
+  private By btnLogin = By.xpath("//form[@id='login-form']//button[@type='submit']");
+  private By messageError = By.id("login-message");
+
+  // retornar driver web
+  private void getDriver() {
+
+  }
+
+  public void login(String usuario, String password) {
+    WebDriver driver = getDriver();
+    driver.findElement(userField).sendKeys(usuario);
+    driver.findElement(passwordField).sendKeys(password);
+    driver.findElement(btnLogin).click();
+  }
+
+  public String obtenerError() {
+    List<WebElement> messageErrors = getDriver().findElements(messageError);
+    return messageErrors.isEmpty()?"": messageErrors.get(0).getText().trim();
+  }
+
+
+
+
 }
