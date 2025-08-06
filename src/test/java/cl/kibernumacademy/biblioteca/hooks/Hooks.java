bfe76f439e79e497cbcd4bd35ad20c34129f3e1c
Miguel Ramos <return.infox@gmail.com>
2025-08-05 20:54:19 -0400

    hooks

--- src/test/java/cl/kibernumacademy/biblioteca/hooks/Hooks.java
+++ src/test/java/cl/kibernumacademy/biblioteca/hooks/Hooks.java
@@ -2,6 +2,8 @@ package cl.kibernumacademy.biblioteca.hooks;
 
 import java.time.Duration;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 import io.cucumber.java.After;
@@ -23,6 +25,13 @@ public class Hooks {
 
   @After
   public void tearDown(Scenario scenario) {
-    
+    // Obtener el driver que se esta usando
+    WebDriver driver = DriverHolder.get();
+    if(scenario.isFailed()) {
+      byte[] shot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+      scenario.attach(shot, "image/png", scenario.getName());
+    }
+    driver.quit(); // Cierra el navegador
+    DriverHolder.remove(); //* Evita las fugas de memoria
   }
 }
